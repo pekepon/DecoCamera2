fba3d305eacbed2345c90d0bfb376a6516a5361b
井上圭一 <inoue@inter-reuse.co.jp>
2016-02-01 08:33:10 +0900

    Initial Commit

---
+++ Decocamera2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Decocamera2
+//
+//  Created by 井上圭一 on 2016/02/01.
+//  Copyright © 2016年 keiichi, inoue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
